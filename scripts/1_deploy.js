20fa84e1b5a3d182e9b09832e4138660a9058699
jackee cen <368435699@qq.com>
2023-02-07 17:17:43 +0800

    1_deploy.js start

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,11 +1,35 @@
 const { ethers } = require("hardhat");
 
 async function main(){
+    console.log('Preparing deployment .... \n')
 
     const Token = await ethers.getContractFactory("Token")
-    const token = await Token.deploy()
-    await token.deployed()
-    console.log(1,`Token address is  ${token.address}`)
+    const Exchange = await ethers.getContractFactory("Exchange")
+
+    const accounts = await ethers.getSigners()
+
+    console.log(`Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}`)
+
+    // Deploy contracts
+    const dapp = await Token.deploy('Dapp University', 'DAPP', '1000000');
+    await dapp.deployed();
+    console.log(`DAPP Deployed to: ${dapp.address}`);
+
+    const mETH = await Token.deploy('mETH', 'mETH', '1000000');
+    await mETH.deployed();   
+    console.log(`mETH Deployed to:${mETH.address}`);
+
+    const mDai = await Token.deploy('mDAI', 'mDAI', '1000000');
+    await mDai.deployed()
+    console.log(`mDai address is  ${mDai.address}`);
+
+    const exchange = await Exchange.deploy(accounts[1].address, 10);    
+    await exchange.deployed();
+    console.log(`Exchange Deployed to: ${exchange.address}`);
+
+    
+    
+
 }
 
 main().then(()=>{
